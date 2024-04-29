ea5f05553862b3a923e2634dd8ad4dd575b79d67
christyjohnbiju <christyjb2003@gmail.com>
2024-04-29 22:27:54 +0530

    footer addes

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import About from "@/components/Pages/About";
 import Main from "@/components/Pages/Main";
 import Speakers from "@/components/Pages/Speakers";
 import Tickets from "@/components/Pages/Tickets";
+import Footer from"@/components/Footer";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
       <About/>
       <Tickets/>
       <Speakers/>
+      <Footer/>
+
+
+      
     </div>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import Image from 'next/image';
+
+const Footer = () => {
+  return (
+    <footer className="container mx-auto px-4 sm:px-6 py-4 md:py-8 text-white bg-slate-950">
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-col pb-5 md:pb-0 md:mr-8">
+          <h1 className="font-serif text-3xl select-none text-primary">PIEVERSE</h1>
+          <p>Providence college of Engineering</p>
+          <p>Providence College Rd, Angadical South, Ala, Chengannur</p>
+          <p>Kerala 689122</p>
+          
+          <div className="flex flex-row justify-evenly items-center pt-3">
+            <SocialIcon
+              network="instagram"
+              url=""
+              bgColor="#9B0000"
+              fgColor="white"
+              style={{ width: "40px", height: "40px" }}
+            />
+            <SocialIcon
+              network="youtube"
+              url=""
+              bgColor="#9B0000"
+              fgColor="white"
+              style={{ width: "40px", height: "40px" }}
+            />
+            <SocialIcon
+              network="facebook"
+              url=""
+              bgColor="#9B0000"
+              fgColor="white"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </div>
+        </div>
+        
+        <div className="overflow-hidden rounded-xl md:mr-8">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3937.3807110727494!2d76.61414617420553!3d9.299493984697854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0622984cfaf3af%3A0xd0320f890b6fca5!2sProvidence%20College%20of%20Engineering%20%26%20School%20of%20Business%2C%20Chengannur!5e0!3m2!1sen!2sin!4v1710087103947!5m2!1sen!2sin"
+            className="h-full w-auto"
+            allowFullScreen=""
+            loading="none"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+
+      <div className="text-center pt-6">
+        <h2 className="text-xs">© Copyright IEEE PIE KERALA SECTION.All Rights Reserved</h2>
+      </div>
+    </footer> 
+  );
+};
+
+export default Footer;
